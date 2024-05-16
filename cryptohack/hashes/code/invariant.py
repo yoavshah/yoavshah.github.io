5dabd30b521b972409ab3359f457cd7510afce14
Yoav <yoav.sh@ironshieldsolutions.com>
2024-05-17 00:49:55 +0300

    added all solutions to hash

--- cryptohack/hashes/code/invariant.py
+++ cryptohack/hashes/code/invariant.py
@@ -3,6 +3,92 @@ import json
 import base64
 import hashlib
 
+import itertools
+import json
+from hashlib import sha512
+
+FLAG = "crypto{????????????????????????????????}"
+
+
+class MyCipher:
+    __NR = 31
+    __SB = [13, 14, 0, 1, 5, 10, 7, 6, 11, 3, 9, 12, 15, 8, 2, 4]
+    __SR = [0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12, 1, 6, 11]
+
+
+    def __init__(self, key):
+        self.__RK = int(key.hex(), 16)
+        self.__subkeys = [[(self.__RK >> (16 * j + i)) & 1 for i in range(16)]
+                          for j in range(self.__NR + 1)]
+
+        self.KEYS = self.__subkeys
+        
+    def __xorAll(self, v):
+        res = 0
+        for x in v:
+            res ^= x
+        return res
+
+    def encrypt(self, plaintext):
+        assert len(plaintext) == 8, "Error: the plaintext must contains 64 bits."
+
+        S = [int(_, 16) for _ in list(plaintext.hex())]
+
+        for r in range(self.__NR):
+            S = [S[i] ^ self.__subkeys[r][i] for i in range(16)]
+            S = [self.__SB[S[self.__SR[i]]] for i in range(16)]
+            X = [self.__xorAll(S[i:i + 4]) for i in range(0, 16, 4)]
+            S = [X[c] ^ S[4 * c + r]
+                 for c, r in itertools.product(range(4), range(4))]
+
+        S = [S[i] ^ self.__subkeys[self.__NR][i] for i in range(16)]
+        return bytes.fromhex("".join("{:x}".format(_) for _ in S))
+
+    
+            
+class MyHash:
+    def __init__(self, content):
+        self.cipher = MyCipher(sha512(content).digest())
+        self.h = b"\x00" * 8
+        self._update(content)
+
+    def _update(self, content):
+        while len(content) % 8:
+            content += b"\x00"
+
+        for i in range(0, len(content), 8):
+            self.h = bytes(x ^ y for x, y in zip(self.h, content[i:i+8]))
+            self.h = self.cipher.encrypt(self.h)
+            self.h = bytes(x ^ y for x, y in zip(self.h, content[i:i+8]))
+
+    def digest(self):
+        return self.h
+
+    def hexdigest(self):
+        return self.h.hex()
+
+
+block_size = 8
+for block_size in range(8, 32, 8):
+    found = False
+    
+    for comb in itertools.product([0x66, 0x67, 0x76, 0x77], repeat=block_size):
+        
+        data = b"".join([x.to_bytes(1, "little") for x in comb])
+        h = MyHash(data).digest()
+        
+        if h == b"\x00"*8:
+            found = True
+            print("Found combination")
+            print(comb)
+            print(data)
+            break
+
+    if found:
+        break
+        
+            
+        
 r = remote('socket.cryptohack.org', 13393)#, level = 'debug')
 
 _ = r.recvline()
@@ -15,9 +101,11 @@ def json_send(hsh):
     r.sendline(request)
 
 
+json_send({"option": "hash", "data": data.hex()})
 
+flag = json_recv()["flag"]
 
 r.close()
 
-
+print(flag)
 

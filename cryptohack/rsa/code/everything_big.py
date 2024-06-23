5d70f4acc757657997900edc638769613dfa410a
Yoav <yoav.sh@ironshieldsolutions.com>
2024-06-23 17:54:56 +0300

    Added some solutions to rsa

---
+++ cryptohack/rsa/code/everything_big.py
@@ -0,0 +1,157 @@
+import math
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes, GCD, isPrime
+
+# https://github.com/orisano/owiener/blob/master/owiener.py
+
+"""
+The MIT License (MIT)
+Copyright (c) 2019 Nao Yonashiro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+from typing import Tuple, Iterator, Iterable, Optional
+
+
+def isqrt(n: int) -> int:
+    """
+    ref: https://en.wikipedia.org/wiki/Integer_square_root
+    
+    >>> isqrt(289)
+    17
+    >>> isqrt(2)
+    1
+    >>> isqrt(1000000 ** 2)
+    1000000
+    """
+    if n == 0:
+        return 0
+
+    # ref: https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Rough_estimation
+    x = 2 ** ((n.bit_length() + 1) // 2)
+    while True:
+        y = (x + n // x) // 2
+        if y >= x:
+            return x
+        x = y
+
+
+def is_perfect_square(n: int) -> bool:
+    """
+    ref: https://hnw.hatenablog.com/entry/20140503
+
+    >>> is_perfect_square(100)
+    True
+    
+    >>> is_perfect_square(2000000000000000000000000000 ** 2)
+    True
+
+    >>> is_perfect_square(2000000000000000000000000000 ** 2 + 1)
+    False
+    """
+    sq_mod256 = (1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0)
+    if sq_mod256[n & 0xff] == 0:
+        return False
+
+    mt = (
+        (9, (1,1,0,0,1,0,0,1,0)),
+        (5, (1,1,0,0,1)),
+        (7, (1,1,1,0,1,0,0)),
+        (13, (1,1,0,1,1,0,0,0,0,1,1,0,1)),
+        (17, (1,1,1,0,1,0,0,0,1,1,0,0,0,1,0,1,1))
+    )
+    a = n % (9 * 5 * 7 * 13 * 17)
+    if any(t[a % m] == 0 for m, t in mt):
+        return False
+
+    return isqrt(n) ** 2 == n
+
+
+def rational_to_contfrac(x: int, y: int) -> Iterator[int]:
+    """
+    ref: https://en.wikipedia.org/wiki/Euclidean_algorithm#Continued_fractions
+    
+    >>> list(rational_to_contfrac(4, 11))
+    [0, 2, 1, 3]
+    """
+    while y:
+        a = x // y
+        yield a
+        x, y = y, x - a * y
+
+
+def contfrac_to_rational_iter(contfrac: Iterable[int]) -> Iterator[Tuple[int, int]]:
+    """
+    ref: https://www.cits.ruhr-uni-bochum.de/imperia/md/content/may/krypto2ss08/shortsecretexponents.pdf (6)
+    """
+    n0, d0 = 0, 1
+    n1, d1 = 1, 0
+    for q in contfrac:
+        n = q * n1 + n0
+        d = q * d1 + d0
+        yield n, d
+        n0, d0 = n1, d1
+        n1, d1 = n, d
+
+
+def convergents_from_contfrac(contfrac: Iterable[int]) -> Iterator[Tuple[int, int]]:
+    """
+    ref: https://www.cits.ruhr-uni-bochum.de/imperia/md/content/may/krypto2ss08/shortsecretexponents.pdf Section.3
+    """
+    n_, d_ = 1, 0
+    for i, (n, d) in enumerate(contfrac_to_rational_iter(contfrac)):
+        if i % 2 == 0:
+            yield n + n_, d + d_
+        else:
+            yield n, d
+        n_, d_ = n, d
+
+
+def attack(e: int, n: int) -> Optional[int]:
+    """
+    ref: https://www.cits.ruhr-uni-bochum.de/imperia/md/content/may/krypto2ss08/shortsecretexponents.pdf Section.4
+    
+    >>> attack(2621, 8927)
+    5
+    >>> attack(6792605526025, 9449868410449)
+    569
+    >>> attack(30749686305802061816334591167284030734478031427751495527922388099381921172620569310945418007467306454160014597828390709770861577479329793948103408489494025272834473555854835044153374978554414416305012267643957838998648651100705446875979573675767605387333733876537528353237076626094553367977134079292593746416875606876735717905892280664538346000950343671655257046364067221469807138232820446015769882472160551840052921930357988334306659120253114790638496480092361951536576427295789429197483597859657977832368912534761100269065509351345050758943674651053419982561094432258103614830448382949765459939698951824447818497599, 109966163992903243770643456296093759130737510333736483352345488643432614201030629970207047930115652268531222079508230987041869779760776072105738457123387124961036111210544028669181361694095594938869077306417325203381820822917059651429857093388618818437282624857927551285811542685269229705594166370426152128895901914709902037365652575730201897361139518816164746228733410283595236405985958414491372301878718635708605256444921222945267625853091126691358833453283744166617463257821375566155675868452032401961727814314481343467702299949407935602389342183536222842556906657001984320973035314726867840698884052182976760066141)
+    4221909016509078129201801236879446760697885220928506696150646938237440992746683409881141451831939190609743447676525325543963362353923989076199470515758399
+    """
+    f_ = rational_to_contfrac(e, n)
+    for k, dg in convergents_from_contfrac(f_):
+        edg = e * dg
+        phi = edg // k
+
+        x = n - phi + 1
+        if x % 2 == 0 and is_perfect_square((x // 2) ** 2 - n):
+            g = edg - phi * k
+            return dg // g
+    return None
+
+
+N = 0xb8af3d3afb893a602de4afe2a29d7615075d1e570f8bad8ebbe9b5b9076594cf06b6e7b30905b6420e950043380ea746f0a14dae34469aa723e946e484a58bcd92d1039105871ffd63ffe64534b7d7f8d84b4a569723f7a833e6daf5e182d658655f739a4e37bd9f4a44aff6ca0255cda5313c3048f56eed5b21dc8d88bf5a8f8379eac83d8523e484fa6ae8dbcb239e65d3777829a6903d779cd2498b255fcf275e5f49471f35992435ee7cade98c8e82a8beb5ce1749349caa16759afc4e799edb12d299374d748a9e3c82e1cc983cdf9daec0a2739dadcc0982c1e7e492139cbff18c5d44529407edfd8e75743d2f51ce2b58573fea6fbd4fe25154b9964d
+e = 0x9ab58dbc8049b574c361573955f08ea69f97ecf37400f9626d8f5ac55ca087165ce5e1f459ef6fa5f158cc8e75cb400a7473e89dd38922ead221b33bc33d6d716fb0e4e127b0fc18a197daf856a7062b49fba7a86e3a138956af04f481b7a7d481994aeebc2672e500f3f6d8c581268c2cfad4845158f79c2ef28f242f4fa8f6e573b8723a752d96169c9d885ada59cdeb6dbe932de86a019a7e8fc8aeb07748cfb272bd36d94fe83351252187c2e0bc58bb7a0a0af154b63397e6c68af4314601e29b07caed301b6831cf34caa579eb42a8c8bf69898d04b495174b5d7de0f20cf2b8fc55ed35c6ad157d3e7009f16d6b61786ee40583850e67af13e9d25be3
+c = 0x3f984ff5244f1836ed69361f29905ca1ae6b3dcf249133c398d7762f5e277919174694293989144c9d25e940d2f66058b2289c75d1b8d0729f9a7c4564404a5fd4313675f85f31b47156068878e236c5635156b0fa21e24346c2041ae42423078577a1413f41375a4d49296ab17910ae214b45155c4570f95ca874ccae9fa80433a1ab453cbb28d780c2f1f4dc7071c93aff3924d76c5b4068a0371dff82531313f281a8acadaa2bd5078d3ddcefcb981f37ff9b8b14c7d9bf1accffe7857160982a2c7d9ee01d3e82265eec9c7401ecc7f02581fd0d912684f42d1b71df87a1ca51515aab4e58fab4da96e154ea6cdfb573a71d81b2ea4a080a1066e1bc3474
+
+d = attack(e, N)
+
+pt = pow(c, d, N)
+decrypted = long_to_bytes(pt)
+
+print(decrypted)
---
+++ cryptohack/rsa/code/inferious_prime.py
@@ -0,0 +1,26 @@
+
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes, GCD
+
+
+
+e = 3
+ct = 39207274348578481322317340648475596807303160111338236677373
+n = 742449129124467073921545687640895127535705902454369756401331
+
+# http://factordb.com/index.php?query=742449129124467073921545687640895127535705902454369756401331
+p = 752708788837165590355094155871
+q = 986369682585281993933185289261
+
+
+phi = (p - 1) * (q - 1)
+d = inverse(e, phi)
+
+pt = pow(ct, d, n)
+decrypted = long_to_bytes(pt)
+
+print(decrypted)
+
+
+
+
+
